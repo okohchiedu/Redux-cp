33e339dc5aac731192942f3d8a7e9465adafb3c6
okohchiedu <okohthomas35@gmail.com>
2023-06-05 17:44:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css'
+import React, { useEffect, useState, useMemo } from 'react';
+import AddTask from './Components/AddTask';
+import ListTask from './Components/ListTask';
+
+
+
+const App = () => {
+  const fonts = useMemo(() => [
+    'Arial',
+    'Verdana',
+    'Helvetica',
+    'Georgia',
+
+  ],
+  []);
+
+  const [randomFont, setRandomFont] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * fonts.length);
+    setRandomFont(fonts[randomIndex]);
+  }, [fonts]);
+  
+  return (
+    <div className='App' style={{ fontFamily: randomFont}}>
+      <h1 style={{marginTop:"10px"}}>TODO APP</h1>
+      <img src={require("../src/assets/logo.jpg")}
+      alt="logo" 
+      className='rotate-image'
+
+      />
+      <AddTask />
+      <ListTask />  
+    </div>
+  );
+};
+
+export default App;
